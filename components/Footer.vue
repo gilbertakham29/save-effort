966f84bb85873e582c3800b1446f180af6eac216
Gilbert <akhamgilbert@gmail.com>
2023-09-20 19:35:56 +0530

    frontend UI for a website

---
+++ components/Footer.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+
+        <footer >
+    <div class="flex flex-col items-center grid-cols-2 gap-10 px-4 py-10 mx-auto sm:justify-around sm:flex-row max-w-screen-2xl sm:px-8">
+        <div>
+          <div class="flex items-center justify-center gap-2 lg:justify-start">
+                  <img src="https://save-effort.netlify.app/_nuxt/logo.6ece8b48.svg" class="w-12" alt="Logo" />
+                  <h4 class="text-xl font-bold">SaveEfforts.com</h4>
+          </div>
+          <p class="mt-3 text-gray-700">Save Efforts & time with automations</p>
+        </div>
+          <div class="flex flex-col items-center gap-5 sm:items-start">
+              
+                  <h5 class="text-lg font-semibold">Get in Touch</h5>
+                  <ul class="flex flex-col items-center gap-5 font-medium text-gray-800 sm:items-start">
+                      <li class="flex items-center gap-2">
+                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m16.344 12.168-1.4-1.4a1.98 1.98 0 0 0-2.8 0l-.7.7a1.98 1.98 0 0 1-2.8 0l-2.1-2.1a1.98 1.98 0 0 1 0-2.8l.7-.7a1.981 1.981 0 0 0 0-2.8l-1.4-1.4a1.828 1.828 0 0 0-2.8 0C-.638 5.323 1.1 9.542 4.78 13.22c3.68 3.678 7.9 5.418 11.564 1.752a1.828 1.828 0 0 0 0-2.804Z"/>
+  </svg> +441908103333
+
+                      </li>
+                      <li class="flex items-center gap-2">
+                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 2-8.4 7.05a1 1 0 0 1-1.2 0L1 2m18 0a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1m18 0v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2"/>
+  </svg> aiautomation@saveefforts.com
+                      </li>
+                      <li class="flex items-center gap-2">
+                        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 21">
+    <g stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+      <path d="M8 12a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"/>
+      <path d="M13.8 12.938h-.01a7 7 0 1 0-11.465.144h-.016l.141.17c.1.128.2.252.3.372L8 20l5.13-6.248c.193-.209.373-.429.54-.66l.13-.154Z"/>
+    </g>
+  </svg>Milton Keyens, United Kingdom
+
+                      </li>
+                  </ul>
+             
+             
+             
+          </div>
+      </div>
+      <div class="flex flex-col flex-wrap justify-between w-full gap-5 px-1 py-5 border-t sm:items-center sm:flex-row sm:px-16 border-t-gray-200">
+        <div class="text-sm font-medium text-gray-500">SaveEfforts.com © 2023 All rights reserved</div>
+        <div class="text-sm font-medium text-gray-500">SaveEfforts is brand name of Qualyval Ltd is a company registered in England & Wales.</div>
+        <a href="/privacy-policy" class="text-sm font-medium text-gray-500">PRIVACY POLICY</a>
+      </div>
+</footer>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style  scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <Header/>
+        <MainSection/>
+        <Footer/>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
