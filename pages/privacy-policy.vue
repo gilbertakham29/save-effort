e42f65a6c3958fc6e187b9c1e5145b4232fbf91a
Gilbert <akhamgilbert@gmail.com>
2023-09-21 01:28:05 +0530

    added privacy policy page

---
+++ pages/privacy-policy.vue
@@ -0,0 +1,107 @@
+<template>
+    <div>
+        <main class="max-w-screen-lg px-4 mx-auto my-10 sm:px-8">
+            <a href="/" class="flex items-center justify-start w-full gap-2 mb-2 text-lg font-semibold sm:justify-end sm:mb-8 group">
+                <span class="text-3xl font-bold -translate-y-0.5 w-5 overflow-hidden group-hover:w-9 duration-200">
+                    ⟵ 
+                </span>
+                back
+            </a>
+            <h2 class="text-4xl font-semibold">Privacy Policy</h2>
+            <span class="mt-5 text-sm font-medium text-gray-500">Effective Date: 23-July-2023</span>
+            <p class="mt-16 font-medium">
+                This Privacy Policy governs the manner in which Qualyval Ltd ("we," "us," or "our") collects, uses, maintains, and discloses information collected from users ("you" or "users") of our website. As a company registered in England and Wales with registration number 12605064, we are committed to protecting your privacy and ensuring the security of your personal information. By using our website, you consent to the practices outlined in this Privacy Policy.
+            </p>
+            <h3 class="mt-10 text-2xl font-semibold">Information We Collect</h3>
+            <p class="mt-3 ">
+                We may collect personal information from users in various ways, including but not limited to when you visit our website, register on the site, fill out a form, subscribe to our newsletter, respond to a survey, or interact with other features or services we make available on our website. The types of personal information we may collect include, but are not limited to:
+            </p>
+            <ul class="mt-5 ml-5 list-disc">
+                <li>Name</li>
+                <li>Contact information (e.g., email address, phone number, postal address)</li>
+                <li>Company or organization name (if applicable)</li>
+                <li>Job title (if applicable)</li>
+                <li>Payment information (if applicable)</li>
+            </ul>
+            <p class="mt-4">
+                Any other information you voluntarily provide to us How We Use Collected Information We may use the personal information we collect from you for the following purposes are given below.
+            </p>
+            <h3 class="mt-10 text-2xl font-semibold">Why we collect?</h3>
+            <div class="mt-6">
+                <h5 class="text-lg font-semibold">
+                    Personalize user experience
+                </h5>
+                <p class="mt-2">
+                    We may use the information to understand how our users as a group use the services and resources provided on our website.
+                </p>
+            </div>
+            <div class="mt-4">
+                <h5 class="text-lg font-semibold">
+                    Send periodic emails
+                </h5>
+                <p class="mt-2">
+                    We may use the email address to send you information and updates related to your inquiries, orders, or other requests. It may also be used to respond to your questions, comments, or inquiries. If you decide to opt-in to our mailing list, you will receive emails that may include company news, updates, related product or service information, etc. If at any time you would like to unsubscribe from receiving future emails, we include detailed unsubscribe instructions at the bottom of each email.
+                </p>
+            </div>
+            <div class="mt-4">
+                <h5 class="text-lg font-semibold">
+                  Process transactions
+                </h5>
+                <p class="mt-2">
+                    If you make a purchase through our website, we may use the information you provide to process the transaction and deliver the purchased product or service. To improve our website: We continually strive to improve our website offerings based on the information and feedback we receive from you.
+                </p>
+            </div>
+            <h3 class="mt-10 text-2xl font-semibold">
+                Protecting Your Information
+            </h3>
+            <p class="mt-4">
+                We adopt appropriate data collection, storage, and processing practices and security measures to protect against unauthorized access, alteration, disclosure, or destruction of your personal information, username, password, transaction information, and data stored on our website.
+            </p>
+            <h3 class="mt-10 text-2xl font-semibold">
+                Sharing your Personal Information
+            </h3>
+            <p class="mt-4">
+                We do not sell, trade, or rent users' personal identification information to others. We may share generic aggregated demographic information not linked to any personal identification information regarding visitors and users with our business partners, trusted affiliates, and advertisers for the purposes outlined above. We may also use third-party service providers to help us operate our business and the website or administer activities on our behalf, such as sending out newsletters or surveys. We may share your information with these third parties for those limited purposes, provided that you have given us your permission.
+            </p>
+            <h3 class="mt-10 text-2xl font-semibold">
+                Compliance with Laws
+            </h3>
+            <p class="mt-4">
+                We will disclose your personal information where required to do so by law or subpoena or if we believe that such action is necessary to comply with the law and the reasonable requests of law enforcement or to protect the security or integrity of our website.
+            </p>
+            <h3 class="mt-10 text-2xl font-semibold">
+                Changes to This Privacy Policy
+            </h3>
+            <p class="mt-4">
+                We reserve the right to update this Privacy Policy at any time. When we do, we will revise the updated date at the top of this page. We encourage users to frequently check this page for any changes to stay informed about how we are helping to protect the personal information we collect. You acknowledge and agree that it is your responsibility to review this Privacy Policy periodically and become aware of modifications.
+            </p>
+            <h3 class="mt-10 text-2xl font-semibold">
+                Your Acceptance of These Terms
+            </h3>
+            <p class="mt-4">
+                By using this website, you signify your acceptance of this Privacy Policy. If you do not agree to this policy, please do not use our website. Your continued use of the website following the posting of changes to this policy will be deemed your acceptance of those changes.
+            </p>
+            <h3 class="mt-10 text-2xl font-semibold">
+                Contact Us
+            </h3>
+            <p class="mt-4">
+                If you have any questions about this Privacy Policy, the practices of this site, or your dealings with this site, please contact us at:
+            </p>
+            <div class="mt-2 mb-10">
+                <p>Qualyval Ltd</p>
+                <p>Milton Keynes, United Kingdom</p>
+                <p>info@qualyval.com</p>
+                <p>+44 190810333</p>
+            </div>
+            <span class="text-sm font-medium text-gray-600">This Privacy Policy was last updated on 23-July-2023</span>
+        </main>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
